71e7ed82ff73a1b0146168e0c70652a28dd74a9c
AnggiSatria <anggisatria122@gmail.com>
2022-08-09 00:54:14 +0700

    init page create users

--- src/components/Users/CreateUsers.jsx
+++ src/components/Users/CreateUsers.jsx
@@ -1,30 +1,70 @@
+import { Button, TextField } from '@mui/material'
 import React, { useState } from 'react'
 
 function CreateUsers() {
 
-  const [login, setLogin] = useState({
-    email: "",
+  const [users, setUsers] = useState({
+    firstName : "",
+    lastName : "",
+    email : "",
+    phone : "",
     password : ""
+
   })
 
   const handleChange = (e) => {
-    setLogin({
-      ...login,
+    setUsers({
+      ...users,
       [e.target.name] : e.target.value
     })
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(login);
+    console.log(users);
   }
 
   return (
     <div>
-        <form action="" onSubmit={handleSubmit}>
-          <input type="text" name='email' value={login.email} onChange={handleChange} placeholder='email'/>
-          <button type='submit'>submit</button>
-        </form>
+      <div className="txt" style={{textAlign : "center"}}>
+        <h4>Create Users</h4>
+      </div>
+      <div className='form' style={{width : "600px", padding : "50px", borderRadius : "5px", margin : "auto"}}>
+                <div className="firstName">
+                    <h4>Fist Name</h4>
+                        <TextField id="outlined-search" label="First Name" type="text" style={{width : "100%"}} name="firstName" value={users.firstName} onChange={handleChange}/>
+                </div>
+
+                <div className="lastName" style={{marginTop : "20px"}}>
+                    <h4>Last Name</h4>
+                    <TextField id="outlined-search" label="Last Name" type="mail" style={{width : "100%"}} name="lastName" value={users.lastName} onChange={handleChange}/>
+                </div>
+
+                <div className="email" style={{marginTop : "20px"}}>
+                    <h4>Email Address</h4>
+                    <TextField id="outlined-search" label="Email" type="mail" style={{width : "100%"}} name="email" value={users.email} onChange={handleChange}/>
+                </div>
+
+                <div className="lastName" style={{marginTop : "20px"}}>
+                    <h4>No Phone</h4>
+                    <TextField id="outlined-search" label="No Phone" type="number" style={{width : "100%"}} name="phone" value={users.phone} onChange={handleChange}/>
+                </div>
+
+                <div className="password" style={{marginTop : "20px"}}>
+                    <h4>Password</h4>
+                    <TextField
+                            id="outlined-password-input"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            style={{width : "100%"}}
+                            name="password"
+                            value={users.password}
+                            onChange={handleChange}
+                    />
+                    <Button type="submit" onClick={handleSubmit} variant="contained" color="primary" style={{marginTop : "20px", width : "100%"}}>Submit</Button>
+                </div>      
+            </div>
     </div>
   )
 }
